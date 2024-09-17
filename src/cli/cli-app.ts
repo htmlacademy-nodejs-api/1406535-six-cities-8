7dd5fb2438256c6d342dae143d97b9a0c32840e9
Irina Lagutina <ivl77@yandex.ru>
2024-09-17 12:06:09 +0300

    Добавили менеджеру метод processCommand и значение по умолчанию: --help

--- src/cli/cli-app.ts
+++ src/cli/cli-app.ts
@@ -1,9 +1,14 @@
 import { Command } from './commands/command.interface.js';
+import { CommandParser } from './command-parser.js';
 
 type CommandList = Record<string, Command>;
 
 export class CLIApp {
   private commands: CommandList = {};
+
+  constructor(
+    private readonly defaultCommand: string = '--help'
+  ) {}
 
   public registerCommands(commandItems: Command[]): void {
     commandItems.forEach((command) => {
@@ -13,5 +18,24 @@ export class CLIApp {
 
       this.commands[command.getName()] = command;
     });
+  }
+
+  public getDefaultCommand(): Command | never {
+    if (!this.commands[this.defaultCommand]) {
+      throw new Error(`The default command (${this.defaultCommand}) is not registered.`);
+    }
+    return this.commands[this.defaultCommand];
+  }
+
+  public getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaultCommand();
+  }
+
+  public processCommand(argv: string[]): void {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+    command.execute(...commandArguments);
   }
 }
