80e0f6131da4769c8c6883113f71cc61d9013c9d
Irina Lagutina <ivl77@yandex.ru>
2024-09-19 18:31:02 +0300

    Добавила приватный метод load в команду генерации данных

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,12 +1,34 @@
+import got from 'got';
 import { Command } from './command.interface.js';
+import { MockServerData } from '../../shared/types/index.js';
 
 export class GenerateCommand implements Command {
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...params: string[]): Promise<void> | void {
+  public async execute(...params: string[]): Promise<void> {
     const [count, filePath, url] = params;
     const offerCount = Number.parseInt(count, 10);
+
+    try {
+      await this.load(url);
+    } catch (err: unknown) {
+      console.error('Can\'t generate offers.');
+
+      if (err instanceof Error) {
+        console.error(`Details: ${err.message}`);
+      }
+    }
   }
 }
