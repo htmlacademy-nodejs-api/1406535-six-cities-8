14c04df592223a356c2eb641dbd03cfb03429c59
Irina Lagutina <ivl77@yandex.ru>
2024-09-23 21:03:23 +0300

    Добавила config в приложение

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,9 +1,12 @@
 import { PinoLogger } from './shared/libs/logger/pino.logger.js';
+import { RestConfig } from './shared/libs/config/rest.config.js';
 import { RESTApp } from './rest/rest.app.js';
 
 async function bootstrap() {
   const logger = new PinoLogger();
-  const app = new RESTApp(logger);
+  const config = new RestConfig(logger);
+
+  const app = new RESTApp(logger, config);
   await app.init();
 }
 
--- src/rest/rest.app.ts
+++ src/rest/rest.app.ts
@@ -1,11 +1,14 @@
 import { PinoLogger } from '../shared/libs/logger/pino.logger.js';
+import { Config } from '../shared/libs/config/config.interface.js';
 
 export class RESTApp {
   constructor(
-    private readonly logger: PinoLogger
+    private readonly logger: PinoLogger,
+    private readonly config: Config,
   ) {}
 
   public async init() {
     this.logger.info('Application initialization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
