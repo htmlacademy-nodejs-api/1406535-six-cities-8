c068a58b9dcf41a0b2e3c1b0ff507077f6bee6d2
Irina Lagutina <ivl77@yandex.ru>
2024-09-23 20:36:06 +0300

    Установила dotenv, обновила RestConfig

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -3,7 +3,6 @@ import { RESTApp } from './rest/rest.app.js';
 
 async function bootstrap() {
   const logger = new PinoLogger();
-
   const app = new RESTApp(logger);
   await app.init();
 }
--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,9 +1,22 @@
+import { DotenvParseOutput, config } from 'dotenv';
 import { Config } from './config.interface.js';
+import { PinoLogger } from '../logger/pino.logger.js';
 
 export class RestConfig implements Config {
+  private readonly config: NodeJS.ProcessEnv;
+
   constructor(
-    private readonly config: NodeJS.ProcessEnv
-  ) {}
+    private readonly logger: PinoLogger
+  ) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env-file.');
+    }
+
+    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    this.logger.info('.env-file found and successfully parsed!');
+  }
 
   public get(key: string): string | undefined {
     return this.config[key];
