d4985584875a00e12c6f0229e2b4f11f56c04e33
Irina Lagutina <ivl77@yandex.ru>
2024-09-25 15:45:29 +0300

    Описала схему и конфигурации. Значения будут передаваться через переменные окружения (устанавливаются на основании .env-файла)

--- src/rest/rest.app.ts
+++ src/rest/rest.app.ts
@@ -1,10 +1,11 @@
 import { PinoLogger } from '../shared/libs/logger/pino.logger.js';
 import { Config } from '../shared/libs/config/config.interface.js';
+import { RestSchema } from '../shared/libs/config/rest.schema.js';
 
 export class RESTApp {
   constructor(
     private readonly logger: PinoLogger,
-    private readonly config: Config,
+    private readonly config: Config<RestSchema>,
   ) {}
 
   public async init() {
