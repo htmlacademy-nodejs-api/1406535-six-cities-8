89c3e8c607173e41b9c1824c257cb9b15c8f3647
Irina Lagutina <ivl77@yandex.ru>
2024-10-06 20:25:38 +0300

    Добавила в контейнер новую привязку DatabaseClient

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -6,6 +6,8 @@ import { Component } from './shared/const.js';
 import { PinoLogger } from './shared/libs/logger/pino.logger.js';
 import { Logger } from './shared/libs/logger/logger.interface.js';
 import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
+import { MongoDatabaseClient } from './shared/libs/database-client/mongo.database-client.js';
+import { DatabaseClient } from './shared/libs/database-client/database-client.interface.js';
 
 async function bootstrap() {
   const container = new Container();
@@ -13,6 +15,7 @@ async function bootstrap() {
   container.bind<RESTApplication>(Component.RestApplication).to(RESTApplication).inSingletonScope();
   container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const application = container.get<RESTApplication>(Component.RestApplication);
   await application.init();
