e16a7e65f451aab35b26cb18822e53cfc4037a4c
Irina Lagutina <ivl77@yandex.ru>
2024-10-06 20:35:15 +0300

    Повторное подключение к базе данных

--- src/shared/libs/database-client/mongo.database-client.ts
+++ src/shared/libs/database-client/mongo.database-client.ts
@@ -1,5 +1,6 @@
 import * as Mongoose from 'mongoose';
 import { inject, injectable } from 'inversify';
+import { setTimeout } from 'node:timers/promises';
 import { DatabaseClient } from './database-client.interface.js';
 import { Logger } from '../logger/logger.interface.js';
 import { Component } from '../../const.js';
@@ -23,9 +24,22 @@ export class MongoDatabaseClient implements DatabaseClient {
     }
 
     this.logger.info('Trying to connect to MongoDB...');
-    this.mongoose = await Mongoose.connect(uri);
-    this.isConnected = true;
-    this.logger.info('Database connection established');
+
+    let attempt = 0;
+    while (attempt < 5) {
+      try {
+        this.mongoose = await Mongoose.connect(uri);
+        this.isConnected = true;
+        this.logger.info('Database connection established');
+        return;
+      } catch (error) {
+        attempt++;
+        this.logger.error(`Failed to connect to the database. Attempt ${attempt}`, error as Error);
+        await setTimeout(1000);
+      }
+    }
+
+    throw new Error('Unable to establish database connection after 5 attempts');
   }
 
   public async disconnect(): Promise<void> {
