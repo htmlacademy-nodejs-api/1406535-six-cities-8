dafe33ef843908904788557df41136c4f17596a5
Irina Lagutina <ivl77@yandex.ru>
2024-10-07 14:43:50 +0300

    Добавит сущность и модель Facility

---
+++ src/shared/modules/facility/facility.entity.ts
@@ -0,0 +1,19 @@
+import { defaultClasses, getModelForClass, modelOptions, prop } from '@typegoose/typegoose';
+import { Facility } from '../../types/index.js';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface FacilityEntity extends defaultClasses.Base { }
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'facilities',
+    timestamps: true,
+  }
+})
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export class FacilityEntity extends defaultClasses.TimeStamps implements Facility {
+  @prop({ required: true, trim: true })
+  public name!: string;
+}
+
+export const CategoryModel = getModelForClass(FacilityEntity);
---
+++ src/shared/modules/facility/index.ts
@@ -0,0 +1,3 @@
+export { CreateFacilityDto } from './dto/create-facility.dto.js';
+export { FacilityService } from './facility-service.interface.js';
+export { FacilityEntity } from './facility.entity.js';
