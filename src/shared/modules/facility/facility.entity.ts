e3777fdedc034fc3e8e31aed4830666c95a7ccfa
Irina Lagutina <ivl77@yandex.ru>
2024-10-07 15:18:29 +0300

    Добавили сущность OfferEntity

--- src/shared/modules/facility/facility.entity.ts
+++ src/shared/modules/facility/facility.entity.ts
@@ -13,7 +13,7 @@ export interface FacilityEntity extends defaultClasses.Base { }
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class FacilityEntity extends defaultClasses.TimeStamps implements Facility {
   @prop({ required: true, trim: true })
-  public name!: string;
+  public name: string;
 }
 
 export const FacilityModel = getModelForClass(FacilityEntity);
