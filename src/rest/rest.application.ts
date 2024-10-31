06a1e346e704ab9c7f825423cfa2a043db582559
Irina Lagutina <ivl77@yandex.ru>
2024-10-31 21:48:03 +0300

    Добавит интерфейс Route

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -5,32 +5,49 @@ import { RestSchema } from '../shared/libs/config/rest.schema.js';
 import { Component } from '../shared/const.js';
 import { DatabaseClient } from '../shared/libs/database-client/database-client.interface.js';
 import { getMongoURI } from '../shared/helpers/database.js';
+import express, { Express } from 'express';
 
 @injectable()
 export class RESTApplication {
+  private readonly server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: PinoLogger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
-  ) { }
+  ) {
+    this.server = express();
+  }
 
-  private async initDb() {
+  private async initDataBase() {
     const mongoUri = getMongoURI(
       this.config.get('DB_USER'),
       this.config.get('DB_PASSWORD'),
       this.config.get('DB_HOST'),
       this.config.get('DB_PORT'),
-      this.config.get('DB_NAME'),
+      this.config.get('DB_NAME')
     );
 
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async initServer() {
+    const port = this.config.get('PORT');
+    this.server.get('/', (_req, res) => {
+      res.send('Hello');
+    });
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+
     this.logger.info('Init database...');
-    await this.initDb();
+    await this.initDataBase();
     this.logger.info('Init database completed');
+
+    this.logger.info('Try to init server...');
+    await this.initServer();
+    this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
