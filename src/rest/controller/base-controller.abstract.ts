6c5ed5ea0b8a0f69be5024bc0c72405a7c1c4522
Irina Lagutina <ivl77@yandex.ru>
2024-10-31 22:03:23 +0300

    Добавит абстрактный класс BaseController

---
+++ src/rest/controller/base-controller.abstract.ts
@@ -0,0 +1,46 @@
+import { injectable } from 'inversify';
+import { Controller } from './controller.interface.js';
+import { Response, Router } from 'express';
+import { Logger } from '../../shared/libs/logger/logger.interface.js';
+import { Route } from '../route.interface.js';
+import { StatusCodes } from 'http-status-codes';
+
+@injectable()
+export abstract class BaseController implements Controller {
+  private readonly DEFAULT_CONTENT_TYPE = 'application/json';
+  private readonly _router: Router;
+
+  constructor(
+    protected readonly logger: Logger
+  ) {
+    this._router = Router();
+  }
+
+  get router() {
+    return this._router;
+  }
+
+  public addRoute(route: Route) {
+    this._router[route.method](route.path, route.handler.bind(this));
+    this.logger.info(`Route registered: ${route.method.toUpperCase()} ${route.path}`);
+  }
+
+  public send<T>(res: Response, statusCode: number, data: T): void {
+    res
+      .type(this.DEFAULT_CONTENT_TYPE)
+      .status(statusCode)
+      .json(data);
+  }
+
+  public created<T>(res: Response, data: T): void {
+    this.send(res, StatusCodes.CREATED, data);
+  }
+
+  public noContent<T>(res: Response, data: T): void {
+    this.send(res, StatusCodes.NO_CONTENT, data);
+  }
+
+  public ok<T>(res: Response, data: T): void {
+    this.send(res, StatusCodes.OK, data);
+  }
+}
