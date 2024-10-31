44f9c5417a5da7c5ee011e46475386cb0a910e63
Irina Lagutina <ivl77@yandex.ru>
2024-10-31 23:40:55 +0300

    Добавит метод login в UserController (пока черновик)

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -3,13 +3,14 @@ import { Response } from 'express';
 import { Component } from '../../const.js';
 import { Logger } from '../../libs/logger/logger.interface.js';
 import { BaseController } from '../../libs/rest/controller/base-controller.abstract.js';
-import { CreateUserRequest } from './create-user-request.type.js';
 import { UserService } from './index.js';
 import { Config, RestSchema } from '../../libs/config/index.js';
 import { HttpError } from '../../libs/rest/http-error.js';
 import { StatusCodes } from 'http-status-codes';
 import { fillDTO } from '../../helpers/common.js';
 import { UserRdo } from './rdo/user.rdo.js';
+import { LoginUserRequest } from './types/login-user-request.type.js';
+import { CreateUserRequest } from './types/create-user-request.type.js';
 
 @injectable()
 export class UserController extends BaseController {
@@ -22,6 +23,7 @@ export class UserController extends BaseController {
     this.logger.info('Register routes for UserController');
 
     this.addRoute({ path: '/register', method: 'post', handler: this.create });
+    this.addRoute({ path: '/login', method: 'post', handler: this.login });
   }
 
   public async create(
@@ -36,5 +38,18 @@ export class UserController extends BaseController {
 
     const result = await this.userService.create(body, this.configService.get('SALT'));
     this.created(res, fillDTO(UserRdo, result));
+  }
+
+  public async login(
+    { body }: LoginUserRequest,
+    _res: Response,
+  ): Promise<void> {
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (!existsUser) {
+      throw new HttpError(StatusCodes.UNAUTHORIZED, `User with email ${body.email} not found.`, 'UserController');
+    }
+
+    throw new HttpError(StatusCodes.NOT_IMPLEMENTED, 'Not implemented', 'UserController');
   }
 }
