433048490b69a38099f0f05695b19ef426ac2f47
Irina Lagutina <ivl77@yandex.ru>
2024-11-01 20:10:50 +0300

    Добавит обработчик show (OfferController)

--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -5,11 +5,14 @@ import { StatusCodes } from 'http-status-codes';
 import { HttpError } from '../../libs/rest/http-error.js';
 import { Logger } from '../../libs/logger/logger.interface.js';
 import { Component } from '../../const.js';
+import { OfferService } from './index.js';
+import { ParamOfferId } from './types/param-offerid.type.js';
 
 @injectable()
 export class OfferController extends BaseController {
   constructor(
     @inject(Component.Logger) logger: Logger,
+    @inject(Component.OfferService) private readonly offerService: OfferService,
   ) {
     super(logger);
     this.logger.info('Register routes for OfferController');
@@ -17,12 +20,15 @@ export class OfferController extends BaseController {
     this.addRoute({ path: '/:offerId', method: 'get', handler: this.show });
   }
 
-  public async show(_req: Request, _res: Response): Promise<void> {
-    throw new HttpError(
-      StatusCodes.NOT_IMPLEMENTED,
-      'Not implemented',
-      'OfferController'
-    );
+  public async show({ params }: Request<ParamOfferId>, res: Response): Promise<void> {
+    const { offerId } = params;
+    const offer = await this.offerService.findById(offerId);
+
+    if (!offer) {
+      throw new HttpError(StatusCodes.NOT_FOUND, `Offer with id ${offerId} not found.`, 'OfferController');
+    }
+
+    this.ok(res, offer);
   }
 
 }
