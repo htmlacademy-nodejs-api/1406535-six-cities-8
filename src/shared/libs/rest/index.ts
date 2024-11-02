dc9784651817ccac211baeb573bf5580029b96ce
Irina Lagutina <ivl77@yandex.ru>
2024-11-02 19:14:49 +0300

    Добавит middleware для проверки наличия документа в коллекции

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -7,3 +7,4 @@ export { Controller } from './controller/controller.interface.js';
 export { BaseController } from './controller/base-controller.abstract.js';
 export { Route } from './route.interface.js';
 export { HttpError } from './http-error.js';
+export { DocumentExistsMiddleware } from './middleware/document-exists.middleware.js';
