f022510806402d4516a80b79c283b4b756c353eb
Irina Lagutina <ivl77@yandex.ru>
2024-11-02 19:39:27 +0300

    Зарегистрирует middleware express.static

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -50,6 +50,7 @@ export class RESTApplication {
 
   private async initMiddleware() {
     this.server.use(express.json());
+    this.server.use('/upload', express.static(this.config.get('UPLOAD_DIRECTORY')));
   }
 
   private async initExceptionFilters() {
