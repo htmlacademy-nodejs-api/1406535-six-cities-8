5613c71292ffcaeb7b1c41034022f6804023406e
Irina Lagutina <ivl77@yandex.ru>
2024-11-03 21:48:25 +0300

    Подключит ParseTokenMiddleware глобально

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -8,6 +8,7 @@ import { getMongoURI } from '../shared/helpers/database.js';
 import express, { Express } from 'express';
 import { ExceptionFilter } from '../shared/libs/rest/exception-filter/exception-filter.interface.js';
 import { Controller } from '../shared/libs/rest/controller/controller.interface.js';
+import { ParseTokenMiddleware } from '../shared/libs/rest/middleware/parse-token.middleware.js';
 
 @injectable()
 export class RESTApplication {
@@ -50,8 +51,10 @@ export class RESTApplication {
   }
 
   private async initMiddleware() {
+    const authenticateMiddleware = new ParseTokenMiddleware(this.config.get('JWT_SECRET'));
     this.server.use(express.json());
     this.server.use('/upload', express.static(this.config.get('UPLOAD_DIRECTORY')));
+    this.server.use(authenticateMiddleware.execute.bind(authenticateMiddleware));
   }
 
   private async initExceptionFilters() {
