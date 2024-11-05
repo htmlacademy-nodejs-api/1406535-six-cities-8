d851830b4de2b17b8333c92f039a2a371a1438ea
Irina Lagutina <ivl77@yandex.ru>
2024-11-05 17:21:20 +0300

    Добавит функцию для трансформации списка ошибок

--- src/shared/modules/user/types/create-user-request.type.ts
+++ src/shared/modules/user/types/create-user-request.type.ts
@@ -1,5 +1,5 @@
 import { Request } from 'express';
-import { RequestBody, RequestParams } from '../../../libs/rest/request.type.js';
+import { RequestBody, RequestParams } from '../../../libs/rest/types/request.type.js';
 import { CreateUserDto } from '../dto/create-user.dto.js';
 
 export type CreateUserRequest = Request<RequestParams, RequestBody, CreateUserDto>;
